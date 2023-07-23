371e7cb0f0cfd48d0d3150bbad1167dea24686c3
sakai-web <eiichiro838@gmail.com>
2023-07-23 19:34:03 +0900

    入力機能追加

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const ul = document.getElementById('ul');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log();
+    add();
+});
+
+function add() {
+    let todoText = input.value;
+    if (todoText) {
+        const li = document.createElement('li');
+        li.innerText = todoText;
+        li.classList.add('list-group-item');
+        ul.appendChild(li);
+        input.value = '';
+    }
+}
