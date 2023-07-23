028551239eb1348a50864aeb57649f0ca17cc510
sakai-web <eiichiro838@gmail.com>
2023-07-23 19:57:54 +0900

    入力データをローカルストレージに保存し表示させる

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const ul = document.getElementById('ul');
 
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+    todos.forEach(todo => {
+        add(todo);
+    })
+}
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     console.log();
     add();
 });
 
-function add() {
+function add(todo) {
     let todoText = input.value;
+
+    if (todo) {
+        todoText = todo;
+    }
+
     if (todoText) {
         const li = document.createElement('li');
         li.innerText = todoText;
         li.classList.add('list-group-item');
         ul.appendChild(li);
         input.value = '';
+        saveDate();
     }
+}
+
+function saveDate() {
+    const lists = document.querySelectorAll('li');
+    let todos = [];
+    lists.forEach(list => {
+        todos.push(list.innerText);
+    });
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
